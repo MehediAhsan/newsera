4cb940f6c3acc7478d93f5516cdbc59ec6df0f94
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-03-16 15:50:55 +0600

    updated select field

--- app/dashboard/addNews/page.jsx
+++ app/dashboard/addNews/page.jsx
@@ -8,13 +8,14 @@ const AddNews = () => {
   return (
     <div className="w-7/12 mx-auto mt-20">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
-        <input {...register("firstName")} className="px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2" placeholder=""/>
-        <select {...register("gender")} className="px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2">
-          <option value="female">female</option>
-          <option value="male">male</option>
-          <option value="other">other</option>
+        <input {...register("headline")} className="px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2" placeholder="Type News Headline"/>
+        <select {...register("type")} className="px-3 py-2 text-sm text-gray-600 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:ring-offset-2 focus:ring-indigo-600 focus:ring-2">
+        <option value="">Choose News Type</option>
+          <option value="female">international</option>
+          <option value="male">sports</option>
+          <option value="other">health</option>
         </select>
-        <input type="submit" className="bg-primary p-2 rounded" />
+        <input type="submit" className="bg-primary p-2 rounded cursor-pointer" />
       </form>
     </div>
   );
