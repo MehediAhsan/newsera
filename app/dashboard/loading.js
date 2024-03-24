17ae5df31b858f42f1404a962272ba679cabebf2
MehediAhsan <mdmehedicse01@gmail.com>
2024-03-24 15:07:45 +0600

    implement loader

---
+++ app/dashboard/loading.js
@@ -0,0 +1,12 @@
+import Loader from '@/components/shared/Loader';
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <>
+            <Loader/>
+        </>
+    );
+};
+
+export default LoadingPage;
